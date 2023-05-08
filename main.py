a1c5c0181a7e8ed871d4beb6821e3f1b10608622
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2023-05-08 13:58:26 +0200

    Use subparsers

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import argparse
 import subprocess
 import uiautomator2 as u2
 import inquirer
-from enum import Enum
 
 
 def adb(command_args):
-    return subprocess.run("adb -s " + device_id + " " + command_args, stdout=subprocess.PIPE, shell=True, universal_newlines=True)
+    return subprocess.run("adb -s " + device_id + " " + command_args,
+                          stdout=subprocess.PIPE,
+                          shell=True,
+                          universal_newlines=True)
 
 
 def select_device():
@@ -19,46 +21,50 @@ def select_device():
     if len(devices) == 1:
         return devices[0].split("\t")[0]
 
-    device_selection = [
-        inquirer.List('device',
-                      message="What device would you like to use?",
-                      choices=devices,
-                      ),
-    ]
+    device_selection = [inquirer.List('device', message="What device would you like to use?", choices=devices)]
     answer = inquirer.prompt(device_selection)
     device = answer['device'].split("\t")[0]
     return device
 
 
-class Scripts(Enum):
-    clear_mail_db = "rmdb"
-    show_layout_bounds = "bounds"
-    login = "login"
+def clear_mail_db():
+    global device_id
+    device_id = select_device()
 
-    def __str__(self):
-        return self.value
+    adb("exec-out run-as com.infomaniak.mail find ./files -name 'Mailbox-*.realm' -exec rm {} \\;")
+
+
+def show_layout_bounds():
+    global device_id
+    device_id = select_device()
+
+    result = adb("shell getprop debug.layout")
+    new_layout_state = "true" if (result.stdout.strip() == "false") else "false"
+    print("Setting show layout bounds to " + new_layout_state)
+    adb("shell setprop debug.layout " + new_layout_state)
+    adb("shell service call activity 1599295570")
 
 
 if __name__ == '__main__':
-    device_id = select_device()
+    device_id = None
 
     parser = argparse.ArgumentParser()  # (description="Arguments for kmail")
-    parser.add_argument('script', type=Scripts, choices=list(Scripts), help="what command to run")
+    subparsers = parser.add_subparsers(dest='cmd', help='sub-command help')
+
+    clear_mail_db_parser = subparsers.add_parser("rmdb")
+
+    bounds_parser = subparsers.add_parser("bounds")
 
     args = parser.parse_args()
 
-    match args.script:
-        case Scripts.clear_mail_db:
-            adb("exec-out run-as com.infomaniak.mail find ./files -name 'Mailbox-*.realm' -exec rm {} \\;")
+    match args.cmd:
+        case "rmdb":
+            clear_mail_db()
 
-        case Scripts.show_layout_bounds:
-            result = adb("shell getprop debug.layout")
-            new_layout_state = "true" if (result.stdout.strip() == "false") else "false"
-            print("Setting show layout bounds to " + new_layout_state)
-            adb("shell setprop debug.layout " + new_layout_state)
-            adb("shell service call activity 1599295570")
+        case "bounds":
+            show_layout_bounds()
 
-        case Scripts.login:
+        case "login":
             d = u2.connect()
             out = d.dump_hierarchy()
             print(out)
