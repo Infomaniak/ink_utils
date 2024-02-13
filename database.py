7c91f2defef8dd54a6e82edba4e652de69f9d227
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-02-13 11:50:42 +0100

    Access settings package name for the last lines of codes that were hardcoded to mail

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import subprocess
 
+import config as config
 from adb import adb, select_device, get_all_devices, close_app, open_app
 from utils import remove_empty_items, select_in_list
 
@@ -16,21 +17,23 @@ def clear_mail_db(args):
     else:
         device_ids = [select_device()]
 
+    package_name = config.get_project("package", "name")
+
     for device_id in device_ids:
         if args.mailbox or args.everything:
-            adb("exec-out run-as com.infomaniak.mail find ./files -name 'Mailbox-*-*.realm*' -exec rm -r {} \\;", device_id)
+            adb(f"exec-out run-as {package_name} find ./files -name 'Mailbox-*-*.realm*' -exec rm -r {{}} \\;", device_id)
 
         if args.mailbox_info or args.everything:
-            adb("exec-out run-as com.infomaniak.mail find ./files -name 'MailboxInfo.realm*' -exec rm -r {} \\;", device_id)
+            adb(f"exec-out run-as {package_name} find ./files -name 'MailboxInfo.realm*' -exec rm -r {{}} \\;", device_id)
 
         if args.user or args.everything:
-            adb("exec-out run-as com.infomaniak.mail find ./files -name 'User-*.realm*' -exec rm -r {} \\;", device_id)
+            adb(f"exec-out run-as {package_name} find ./files -name 'User-*.realm*' -exec rm -r {{}} \\;", device_id)
 
         if args.coil or args.everything:
-            adb("exec-out run-as com.infomaniak.mail find ./cache -name '*_cache' -exec rm -r {} \\;", device_id)
+            adb(f"exec-out run-as {package_name} find ./cache -name '*_cache' -exec rm -r {{}} \\;", device_id)
 
         if args.network or args.everything:
-            adb("exec-out run-as com.infomaniak.mail find ./files -name 'network-response-body-*' -exec rm {} \\;", device_id)
+            adb(f"exec-out run-as {package_name} find ./files -name 'network-response-body-*' -exec rm {{}} \\;", device_id)
 
         if args.restart:
             close_app(device_id)
@@ -44,7 +47,9 @@ def open_db(args):
     select_columns = "awk '{print $8, $5, $6, $7}'"
     keep_db = f"grep -x '{get_db_pattern(args)}'"
 
-    result = adb(f"shell run-as com.infomaniak.mail {ls_files} | {select_columns} | {keep_db}", device_id)
+    package_name = config.get_project("package", "name")
+
+    result = adb(f"shell run-as {package_name} {ls_files} | {select_columns} | {keep_db}", device_id)
     files = remove_empty_items(result.stdout.split("\n"))
 
     filename = select_in_list("Select database", files).split(" ")[0]
@@ -54,7 +59,7 @@ def open_db(args):
         shutil.rmtree(working_directory)
     os.makedirs(working_directory, exist_ok=True)
 
-    pull_local_file(f"./files/{filename}", f"{working_directory}/{filename}", device_id)
+    pull_local_file(f"./files/{filename}", f"{working_directory}/{filename}", package_name, device_id)
 
     subprocess.Popen(("open", working_directory + filename), cwd=None)
 
@@ -69,12 +74,14 @@ def get_db_pattern(args):
 
 
 def pull_local_dir(src_path, dest_path, device_id):
-    result = adb(f"exec-out run-as com.infomaniak.mail ls -1 {src_path}", device_id)
+    package_name = config.get_project("package", "name")
+
+    result = adb(f"exec-out run-as {package_name} ls -1 {src_path}", device_id)
     os.makedirs(dest_path, exist_ok=True)
     files = remove_empty_items(result.stdout.split("\n"))
     for file in files:
-        pull_local_file(f"{src_path}/{file}", f"{dest_path}/{file}", device_id)
+        pull_local_file(f"{src_path}/{file}", f"{dest_path}/{file}", package_name, device_id)
 
 
-def pull_local_file(src_path, dest_path, device_id):
-    adb(f"exec-out run-as com.infomaniak.mail cat '{src_path}' > {dest_path}", device_id)
+def pull_local_file(src_path, dest_path, package_name, device_id):
+    adb(f"exec-out run-as {package_name} cat '{src_path}' > {dest_path}", device_id)
