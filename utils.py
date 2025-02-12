0e26e54fc91ed2b83e94dcc3afebb2061dd1805a
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-02-12 09:38:46 +0100

    feat: Support inquirer theme customization by the user

--- utils.py
+++ utils.py
@@ -1,8 +1,32 @@
+import importlib.util
 import os
 
 import inquirer
+from inquirer.themes import Default as DefaultTheme
 
 ink_folder = os.path.abspath(os.path.dirname(__file__))
+
+# Custom overridable theme
+CUSTOM_CLASS_FILE = ink_folder + "/inquirer_theme.py"
+CLASS_NAME = "InkTheme"
+CurrentInkTheme = DefaultTheme
+
+if os.path.isfile(CUSTOM_CLASS_FILE):
+    spec = importlib.util.spec_from_file_location("inquirer_theme", CUSTOM_CLASS_FILE)
+    print(spec)
+    custom_module = importlib.util.module_from_spec(spec)
+    print(custom_module)
+    spec.loader.exec_module(custom_module)
+
+    print("hasattr(custom_module, CLASS_NAME):", hasattr(custom_module, CLASS_NAME))
+
+    # Check if the user defined the class
+    if hasattr(custom_module, CLASS_NAME):
+        CurrentInkTheme = getattr(custom_module, CLASS_NAME)
+    else:
+        pass
+else:
+    pass
 
 
 def remove_empty_items(array):
@@ -12,7 +36,7 @@ def remove_empty_items(array):
 def select_in_list(message, choices):
     if len(choices) == 1:
         return choices[0]
-    return inquirer.prompt([inquirer.List('choice', message=message, choices=choices)])['choice']
+    return inquirer.prompt([inquirer.List('choice', message=message, choices=choices)], theme=CurrentInkTheme())['choice']
 
 
 def accept_substitution(input):
